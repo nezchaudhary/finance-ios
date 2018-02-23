8e6ee59170d4dbe8f10bc9427638b60e01063381
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-02-23 11:57:29 -0800

    Initial modification

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, Platform, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
+        <Text> Here </Text>
       </View>
     );
   }
